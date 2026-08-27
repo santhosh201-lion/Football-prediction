ceef980dc2fec2f7c3c17b1b4f8211d9f28ef2db
santhosh201-lion <santhoshthelion201@gmail.com>
2026-08-27 20:00:04 +0530

    updated leaderboard

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3nTubsptTVQtgGpBiWhFDJHXV4eP0sk8",
+  authDomain: "tournament-predictor-e2a4e.firebaseapp.com",
+  projectId: "tournament-predictor-e2a4e",
+  storageBucket: "tournament-predictor-e2a4e.firebasestorage.app",
+  messagingSenderId: "778406319974",
+  appId: "1:778406319974:web:ae13a7531d58fc66609aa7",
+  measurementId: "G-5KGF2S1KYR"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
